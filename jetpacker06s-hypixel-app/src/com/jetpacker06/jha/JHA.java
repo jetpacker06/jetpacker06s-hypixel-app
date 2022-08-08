86abb54c366a94d434a547e519326f4fce344276
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2022-08-08 16:23:07 -0500

    Key security

--- jetpacker06s-hypixel-app/src/com/jetpacker06/jha/JHA.java
+++ jetpacker06s-hypixel-app/src/com/jetpacker06/jha/JHA.java
@@ -10,8 +10,8 @@ import java.util.concurrent.ExecutionException;
 @SuppressWarnings("deprecation")
 public class JHA {
     public static UUID myUUID = UUID.fromString("692c31c4-de79-418b-9f50-9e27ac540092");
-    public static String key = System.getProperty("apiKey", "b97a8655-29a9-4d20-aa21-d4dbe418e143");
-    public static HypixelAPI API = new HypixelAPI(new ApacheHttpClient(UUID.fromString(key)));
+    public static String KEY = System.getenv("HypixelAPIKey");
+    public static HypixelAPI API = new HypixelAPI(new ApacheHttpClient(UUID.fromString(KEY)));
     public static void log(Object o) {
         System.out.println(o);
     }
